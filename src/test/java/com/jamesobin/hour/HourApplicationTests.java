60838accaa587634fbb95212bd6ebb7b90601b42
jamesobin <jamesobin@naver.com>
2024-12-24 17:56:38 +0900

    [권오빈]  - 프로젝트 생성

---
+++ src/test/java/com/jamesobin/hour/HourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesobin.hour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
