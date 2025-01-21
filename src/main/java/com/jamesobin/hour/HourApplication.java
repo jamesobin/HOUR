80773636a1be9cfd8407de3bb6cc35209e1f5d1d
jamesobin <jamesobin@naver.com>
2025-01-21 18:25:13 +0900

    [권오빈]  - aws 배포 코드 적용

--- src/main/java/com/jamesobin/hour/HourApplication.java
+++ src/main/java/com/jamesobin/hour/HourApplication.java
@@ -2,9 +2,10 @@ package com.jamesobin.hour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class HourApplication {
+public class HourApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HourApplication.class, args);
